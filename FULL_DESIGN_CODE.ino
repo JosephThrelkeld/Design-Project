56faf6334fe1819e3ce0ec895586866fee91d364
JosephThrelkeld <65369227+JosephThrelkeld@users.noreply.github.com>
2021-04-28 18:59:00 -0600

    Update FULL_DESIGN_CODE.ino
    refocused and restructured. Got user control and notification system working

--- FULL_DESIGN_CODE.ino
+++ FULL_DESIGN_CODE.ino
@@ -3,125 +3,172 @@
 #include <SoftwareSerial.h>
 #include "sms.h"
 
-//GPS Libraries
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
+
 #include <TinyGPS++.h>
-class Controller {
-  public:
-    Controller();
-    void manualCheckToSwitch();
-    void toMalfuncMode();
-    void toNormMode();
-    void toEMS_Mode();
-    void turnOff();
-  private:
-    enum mode {
-      off,
-      normalMode,
-      EMS_Mode,
-      malfunc_Mode
-    }
-    enum mode currMode;
-}
-class Button {
-  public:
-    Button();
-    void checkButton();
-    void stateChange();
-    void manualCheckToSwitch();
-    bool getDoubleTap();
-    HoldType getCurrHoldType();
-  private:
-    bool doubleTap;
-    enum HoldType currHoldType;
-    int buttonState = 0;
-    int lastState = 0;
-    int currentState = 0;
-    int pressStart = 0;
-    int pressEnd = 0;
-    int holdTime = 0;
-    int unPressedTime = 0;
-    int currTime = 0;
-    HoldType
-}
+
+int countDownStart;
+
+void checkButton();
+void button_StateChange();
+void manualCheckToSwitch();
+int buttonState = 0;
+int lastState = 0;
+int currentState = 0;
+int pressStart = 0;
+int pressEnd = 0;
+int holdTime = 0;
+int unPressedTime = 0;
+unsigned long currTime = 0;
 
 enum HoldType {
   noPress,
   noHold, //<1 sec
   oneSecHold, // 1< <3 sec
   threeSecHold  // 3< sec
-}
-
-
-//GSM objs
-SMSGSM sms;
-int numdata;
-boolean GSM_started = false;
-char smsbuffer[160];
-char n[20];
-
-
-//GPS objs
-TinyGPSPlus gps;
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 4800;
-SoftwareSerial ss(RXPin, TXPin);
-
-//Button objs
-
-
+};
 bool doubleTap = false;
-enum HoldType currHoldType = noHold;
+HoldType currHoldType = noPress;
+
+unsigned long currLEDTime = 0;
+unsigned long previousLEDTime = 0;
+const unsigned int emsInterval = 250;
+int ledState = LOW;
+
+unsigned long currSpeakerTime = 0;
+unsigned long previousSpeakerTime = 0;
+bool outputTone = false;
+const int emsTone = 262;
+
+enum Mode {
+  off,
+  normalMode,
+  EMS_Mode,
+  malfuncMode
+};
+Mode mode = off;
 
 
-//state var and enum
-
-
-//Pin assignment
-//GSM.cpp defines gsm pins as tx = 2, rx = 3
 static const int buttonPin = 7;
 static const int speakerPin = 8;
 static const int ledPin = 9;
-static const int gpsRXpin = 10;
-static const int gpsTXpin = 11;
 
 void setup() {
   Serial.begin(9600);
-  //Button setup
-  pinmode(7, INPUT);
-  //Speaker setup
-  pinmode(8, OUTPUT);
-  //LED setup
-  pinmode(9, OUTPUT);
-  //GPS setup
-  ss.begin(GPSBaud);
-  //SMS setup
-  //Heart rate monitor setup
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
 }
 
 void loop() {
   checkButton();
   switch (mode) {
-    case normalMode:
-    case EMS_Mode:
-    case malfunc_mode:
     case off:
+      Serial.println("off");
+      if (ledState != LOW) {
+        ledState = LOW;
+        digitalWrite(ledPin, ledState);
+      }
+      break;
+    case normalMode:
+      Serial.println("normalMode");
+      if (ledState != HIGH) {
+        ledState = HIGH;
+        digitalWrite(ledPin, ledState);
+      }
+      break;
+    case EMS_Mode:
+      Serial.println("EMS_Mode");
+      currTime = millis();
+      currLEDTime = millis();
+      currSpeakerTime = millis();
+      
+      if (currLEDTime - previousLEDTime >= emsInterval) {
+        previousLEDTime = currLEDTime;
 
+        if (ledState == LOW) {
+          ledState = HIGH;
+        } else {
+          ledState = LOW;
+        }
+        digitalWrite(ledPin, ledState);
+      }
+      
+      if (outputTone) {
+        if (currSpeakerTime - previousSpeakerTime >= emsInterval) {
+          previousSpeakerTime = currSpeakerTime;
+          noTone(speakerPin);
+          outputTone = false;
+        }
+      } else if (currSpeakerTime - previousSpeakerTime >= emsInterval) {
+        previousSpeakerTime = currSpeakerTime;
+        tone(speakerPin, emsTone);
+        outputTone = true;
+
+      }
+      
+      if (countDownStart == 0) countDownStart = millis();
+      else if (currTime - countDownStart > 30000) {
+        //send sms
+        noTone(speakerPin);
+        outputTone = false;
+        countDownStart = 0;
+        mode = normalMode;
+      }
+      break;
+      
+    case malfuncMode:
+      Serial.println("malfuncMode");
+      break;
   }
 }
 
-//Button
-void Button::checkButton() {
-  buttonState = digitalRead(7);
-  if (buttonState != lastState)return buttonStateChange();
+void toMalfuncMode() {
+  mode = malfuncMode;
+}
 
-  if (pressStart != 0) {
+void turnOff() {
+  tone(speakerPin, 262);
+  delay(100);
+  noTone(speakerPin);
+  delay(100);
+  tone(speakerPin, 131);
+  delay(100);
+  noTone(speakerPin);
+  mode = off;
+}
+
+void toEMS_Mode() {
+  mode = EMS_Mode;
+}
+
+void toNormMode() {
+  tone(speakerPin, 262);
+  delay(100);
+  noTone(speakerPin);
+  delay(100);
+  tone(speakerPin, 262);
+  delay(100);
+  noTone(speakerPin);
+  mode = normalMode;
+}
+
+void checkButton() {
+  buttonState = digitalRead(7);
+  if (buttonState != lastState)buttonStateChange();
+
+  if (buttonState == HIGH) {
     currTime = millis();
     holdTime = currTime - pressStart;
     if (holdTime >= 3000) {
+      Serial.println("threeSecHold");
       currHoldType = threeSecHold;
     } else if (holdTime >= 1000) {
+      Serial.println("oneSecHold");
       currHoldType = oneSecHold;
     } else {
+      Serial.println("noHold");
       currHoldType = noHold;
     }
   }
@@ -130,7 +177,7 @@ void Button::checkButton() {
   lastState = buttonState;
 }
 
-void Button::stateChange() {
+void buttonStateChange() {
   if (buttonState == HIGH) {
     pressStart = millis();
     unPressedTime = pressStart - pressEnd;
@@ -145,13 +192,16 @@ void Button::stateChange() {
   }
 }
 
-void Controller::manualCheckToSwitch() {
-  if (doubleTap && currMode == normalMode) turnOff();
+
+void manualCheckToSwitch() {
+  if (doubleTap && mode == normalMode) turnOff();
   switch (currHoldType) {
     case noHold:
       break;
     case threeSecHold:
       if (mode == normalMode) toEMS_Mode();
+      currHoldType = noPress;
+      break;
     case oneSecHold:
       if (mode == EMS_Mode || mode == off) toNormMode();
     default:
@@ -159,49 +209,3 @@ void Controller::manualCheckToSwitch() {
       break;
   }
 }
-
-//States
-void EMS_State();
-void
-
-//GSM
-void sendEMS_SMS();
-
-//GPS
-String getGPS_Coords();
-
-//Heartbeat sensor
-int getHeartBeat();
-
-//Switch States
-void toMalfuncMode() {
-  mode = malfuncMode;
-}
-
-void turnOff() {
-  mode = off;
-}
-
-void toEMS_Mode() {
-  mode = EMS_Mode;
-}
-
-void toNormMode() {
-  mode = normalMode;
-}
-
-//Notifications:
-
-//LED
-void LED_EMS_Blink();
-void LED_Malfunc_Tone();
-void LED_On();
-void LED_Off();
-
-//Speaker
-void normTone();
-void EMS_Tone();
-void off_Tone();
-void malfunc_Tone();
-
-
